6bd340a3cb1e58eae421fa78e7cf568c585f206a
MONE'M <60390230+Ahmed1monm@users.noreply.github.com>
2023-08-25 02:31:51 +0300

    allow all origns

--- index.php
+++ index.php
@@ -11,6 +11,11 @@ set_exception_handler("ErrorHandler::handleException");
 
 header("Content-type: application/json; charset=UTF-8");
 
+// Set CORS headers to allow all origins
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 
 if ($parts[3] != "products") {
